a33e25088fe6d4a053d2e35d9f372b63c934ea79
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-12-15 01:03:17 +0800

    fix off by one indexes

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,12 @@ mod args;
 fn main() -> Result<(), Box<dyn Error>> {
     let Args { file, start, end } = Args::parse();
     let contents = fs::read_to_string(file)?;
+    let start = start.unwrap_or_default().max(1) - 1;
+    let end = end.unwrap_or(usize::MAX).max(1) - 1;
     let lines: Vec<_> = contents
         .lines()
         .enumerate()
-        .filter(|&(index, _)| index >= start.unwrap_or_default() && index <= end.unwrap_or(usize::MAX))
+        .filter(|&(index, _)| index >= start && index <= end)
         .map(|(_, line)| line)
         .collect();
     print!("{}", lines.join("\n"));
