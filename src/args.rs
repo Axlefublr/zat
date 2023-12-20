ce0604ef1a6c1ebcdf2f8b08db734b53806fac93
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-12-20 18:15:33 +0800

    use /// for about

--- src/args.rs
+++ src/args.rs
@@ -1,15 +1,11 @@
 use clap::Parser;
 use std::path::PathBuf;
 
-mod about {
-    pub const ABOUT: &str = "\
-Print a range of lines of a file.
-Both --start and --end are inclusive.
-Line numbers are 1 indexed, but 0 also means 1.";
-}
-
+/// Print a range of lines of a file.
+/// Both --start and --end are inclusive.
+/// Line numbers are 1 indexed, but 0 also means 1."
 #[derive(Parser)]
-#[command(author, version, about, long_about = about::ABOUT)]
+#[command(author, version)]
 pub struct Args {
     pub file: Option<PathBuf>,
     #[arg(short, long, value_name = "NUM")]
